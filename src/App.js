9f15b43161ed9dcde0072a7abbc7953a484b47ea
71398_Kunal <gajbhiye85@gmail.com>
2023-05-22 16:44:18 +0530

    first assignment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// Create a react-app that displays the following.
+// i) Display Hello World from App components
+// ii) A user-defined component (tag) to display Hello World.
+// Display this tag from App component
 function App() {
   return <h1>Hello World</h1>;
 }
