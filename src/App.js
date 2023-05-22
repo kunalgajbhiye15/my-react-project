1e6d49e41c1725141590a9cbce71a4794ec873f2
71398_Kunal <gajbhiye85@gmail.com>
2023-05-22 17:22:48 +0530

    Assignment 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-// Create a react-app that displays the following
-// iii) A user-defined component that has a local variable text .The component must display this
-// text as a main heading using interpolation.
+// iv) A user-defined component that has two local variables text and style where text is any
+// string and style contains css style property . The component must display this text as a main
+// heading with the style applied using interpolation.
+// Display this tag from App component
+
+import BootstrapDemo from "./pages/bootstrap.js";
+
+// return JSX.
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>;<h1>InterPolation Demo</h1>
+      <h1 style={{ color: "blue" }}>Interpolation Demo</h1>
       <hr />
       <InterpolationBasics />
     </div>
@@ -13,17 +18,22 @@ function App() {
 
 function InterpolationBasics() {
   //1.  Data Member
-  let title = "Hello World";
-  let para = `Vikas,Kamlesh,Krushna ipsum dolor sit amet consectetur adipisicing elit. Pariatur, optio
+  let title = "InterPolation DEMO";
+  let para = `Kamlesh, Krushna, Vikas ipsum dolor sit amet consectetur adipisicing elit. Pariatur, optio
               repellendus delectus velit, est eaque nihil non, et alias dicta animi
               asperiores ullam placeat odit autem sunt voluptates impedit repudiandae?`;
+
+  let mystyle = { color: "red" };
   //2. member function, coming soon...
 
   // 3. UI / VIEW :: JSX
   return (
     <div>
-      <h1>{title}</h1>
-      <p>{para}</p>
+      <h1 style={mystyle}>{title}</h1>
+
+      <p style={{ background: "green", color: "white", padding: "16px" }}>
+        {para}
+      </p>
     </div>
   );
 }
